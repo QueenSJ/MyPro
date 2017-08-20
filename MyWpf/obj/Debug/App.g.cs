1c291806d142582a9824a488538df092e5d37c33
sujeong <sujeong2341@google.co.kr>
2017-08-20 22:09:01 +0900

    Issue-1 #comment 기본 디자인 생성

--- MyWpf/obj/Debug/App.g.cs
+++ MyWpf/obj/Debug/App.g.cs
@@ -61,7 +61,7 @@ namespace MyWpf {
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.0.0.0")]
         public static void Main() {
-            MyWpf.App app = new MyWpf.App();
+            App app = new App();
             app.InitializeComponent();
             app.Run();
         }
