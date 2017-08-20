03cf8875f7161869f85d866dcbb8bf074450446c
sujeong <sujeong2341@google.co.kr>
2017-08-20 22:12:49 +0900

    My-1 #comment 기본 UI 생성

---
+++ MyWpf/MyWpf.Model/GorundModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWpf.MyWpf.Model {
+	class GorundModel {
+		public int XBox {
+			get { return XBox; }
+			set { this.XBox = value; }
+		}
+
+		public int YBox {
+			get { return YBox; }
+			set { this.YBox = value; }
+		}
+
+		public int LandMine {
+			get { return LandMine; }
+			set { this.LandMine = value; }
+		}
+
+		public int[][] Ground {
+			get { return Ground; }
+			set { this.Ground = value; }
+		}
+
+	}
+}
